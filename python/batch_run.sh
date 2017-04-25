9fa1c2352eea53d515fbb2feb0c2505e94e4e756
yujin <yujin@code-sjtu10.weng.kddilabs.jp>
2017-04-25 11:16:39 +0900

    modified batch_run.sh

--- python/batch_run.sh
+++ python/batch_run.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-#data_dir="$HOME/ts_prediction_keras/data/"
 for pp in 0 0.00001 0.0001 0.001 0.01 0.1
 do
 	logdir="../log/pp_"${pp}
 	mkdir -p ${logdir}
-	for target_obj in Patient_2 Dog_1 Dog_2 Dog_3 Dog_4 Dog_5 Patient_1
+	for target_obj in Patient_1 Patient_2
 	do
-#		python run.py $target_obj --corr_coef_pp=${pp} --data_dir=${data_dir} --logdir=${logdir} 2>&1 | tee ${logdir}/log_${target_obj}.txt
+		python run.py $target_obj --win_size=20000 --corr_coef_pp=${pp} --logdir=${logdir} 2>&1 | tee ${logdir}/log_${target_obj}.txt
+	done
+	for target_obj in Dog_1 Dog_2 Dog_3 Dog_4 Dog_5
+	do
 		python run.py $target_obj --corr_coef_pp=${pp} --logdir=${logdir} 2>&1 | tee ${logdir}/log_${target_obj}.txt
 	done
 done
