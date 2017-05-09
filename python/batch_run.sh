288fe461abbe1fb65f0fe90a4695feac712272b9
yujin tang <lerrytang@gmail.com>
2017-05-09 15:52:07 +0900

    add rand_scale_sampling flag

--- python/batch_run.sh
+++ python/batch_run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-for pp in 0 0.00001 0.0001 0.001 0.01 0.1
+for pp in 0.00001 0.0001 0.001 0.01 0.1 1 10 100 1000
 do
-	logdir="../log/pp_"${pp}
+	logdir="../log/rand_scale_pp_"${pp}
 	mkdir -p ${logdir}
-	for target_obj in Patient_1 Patient_2
-	do
-		python run.py $target_obj --win_size=20000 --corr_coef_pp=${pp} --logdir=${logdir} 2>&1 | tee ${logdir}/log_${target_obj}.txt
-	done
 	for target_obj in Dog_1 Dog_2 Dog_3 Dog_4 Dog_5
 	do
-		python run.py $target_obj --corr_coef_pp=${pp} --logdir=${logdir} 2>&1 | tee ${logdir}/log_${target_obj}.txt
+		python run.py $target_obj --corr_coef_pp=${pp} --data_rebalance --rand_crop_sampling --logdir=${logdir} 2>&1 | tee ${logdir}/log_${target_obj}.txt
+	done
+	for target_obj in Patient_1 Patient_2
+	do
+		python run.py $target_obj --downsample=10 --corr_coef_pp=${pp} --data_rebalance --rand_crop_sampling --logdir=${logdir} 2>&1 | tee ${logdir}/log_${target_obj}.txt
 	done
 done
