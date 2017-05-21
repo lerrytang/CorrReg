da0f29e8d3832dc1196c0db768f8307a216362a9
yujin tang <lerrytang@gmail.com>
2017-05-21 12:12:41 +0900

    modified batch_run.sh

--- python/batch_run.sh
+++ python/batch_run.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 
-# baseline
-logdir="../log/baseline"
-mkdir -p ${logdir}
-python run.py Dog_1 --corr_coef_pp=${pp} --n_folds=4 --logdir=${logdir} 2>&1 | tee ${logdir}/log_Dog_1.txt
-python run.py Dog_2 --corr_coef_pp=${pp} --n_folds=7 --logdir=${logdir} 2>&1 | tee ${logdir}/log_Dog_2.txt
-python run.py Dog_3 --corr_coef_pp=${pp} --n_folds=12 --logdir=${logdir} 2>&1 | tee ${logdir}/log_Dog_3.txt
-python run.py Dog_4 --corr_coef_pp=${pp} --n_folds=17 --logdir=${logdir} 2>&1 | tee ${logdir}/log_Dog_4.txt
-python run.py Dog_5 --corr_coef_pp=${pp} --n_folds=5 --logdir=${logdir} 2>&1 | tee ${logdir}/log_Dog_5.txt
-for target_obj in Patient_1 Patient_2
+## baseline + corr_reg
+for pp in 0 0.00001 0.0001 0.001 0.01 0.1
 do
-	python run.py $target_obj --downsample=10 --corr_coef_pp=${pp} --logdir=${logdir} 2>&1 | tee ${logdir}/log_${target_obj}.txt
+	logdir="../log/baseline_"${pp}
+	mkdir -p ${logdir}
+	for target_obj in Dog_1 Dog_2 Dog_3 Dog_4 Dog_5
+	do
+		python run.py $target_obj --corr_reg=${pp} --logdir=${logdir} 2>&1 | tee ${logdir}/log_${target_obj}.txt
+	done
+	for target_obj in Patient_1 Patient_2
+	do
+		python run.py $target_obj --downsample=10 --corr_reg=${pp} --logdir=${logdir} 2>&1 | tee ${logdir}/log_${target_obj}.txt
+	done
 done
 
 
-# multi-scale + cor_reg
-for pp in 0.000001 0.00001 0.0001 0.001 0.01 0.1
+# multi-scale + corr_reg
+for pp in 0 0.00001 0.0001 0.001 0.01 0.1
 do
-	logdir="../log/multiscale_pp_"${pp}
+	logdir="../log/multiscale_"${pp}
 	mkdir -p ${logdir}
-	python run.py Dog_1 --corr_coef_pp=${pp} --multiscale --n_folds=4 --logdir=${logdir} 2>&1 | tee ${logdir}/log_Dog_1.txt
-	python run.py Dog_2 --corr_coef_pp=${pp} --multiscale --n_folds=7 --logdir=${logdir} 2>&1 | tee ${logdir}/log_Dog_2.txt
-	python run.py Dog_3 --corr_coef_pp=${pp} --multiscale --n_folds=12 --logdir=${logdir} 2>&1 | tee ${logdir}/log_Dog_3.txt
-#	python run.py Dog_4 --corr_coef_pp=${pp} --multiscale --n_folds=17 --logdir=${logdir} 2>&1 | tee ${logdir}/log_Dog_4.txt
-#	python run.py Dog_5 --corr_coef_pp=${pp} --multiscale --n_folds=5 --logdir=${logdir} 2>&1 | tee ${logdir}/log_Dog_5.txt
-#	for target_obj in Patient_1 Patient_2
-#	do
-#		python run.py $target_obj --downsample=10 --corr_coef_pp=${pp} --multiscale --logdir=${logdir} 2>&1 | tee ${logdir}/log_${target_obj}.txt
-#	done
+	for target_obj in Dog_1 Dog_2 Dog_3 Dog_4 Dog_5
+	do
+		python run.py $target_obj --corr_reg=${pp} --multiscale --logdir=${logdir} 2>&1 | tee ${logdir}/log_${target_obj}.txt
+	done
+	for target_obj in Patient_1 Patient_2
+	do
+		python run.py $target_obj --downsample=10 --corr_reg=${pp} --multiscale --logdir=${logdir} 2>&1 | tee ${logdir}/log_${target_obj}.txt
+	done
 done
