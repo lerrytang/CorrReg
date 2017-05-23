68de3e9d3b0ec32eb98f599ce2c2cee3a3549e63
yujin tang <lerrytang@gmail.com>
2017-05-23 13:40:53 +0900

    to exclude n_folds flag

--- python/batch_run.sh
+++ python/batch_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## baseline + corr_reg
+# baseline + corr_reg
 for pp in 0 0.00001 0.0001 0.001 0.01 0.1
 do
 	logdir="../log/baseline_"${pp}
@@ -23,10 +23,10 @@ do
 	mkdir -p ${logdir}
 	for target_obj in Dog_1 Dog_2 Dog_3 Dog_4 Dog_5
 	do
-		python run.py $target_obj --corr_reg=${pp} --multiscale --logdir=${logdir} 2>&1 | tee ${logdir}/log_${target_obj}.txt
+		python run.py $target_obj --multiscale --corr_reg=${pp} --logdir=${logdir} 2>&1 | tee ${logdir}/log_${target_obj}.txt
 	done
 	for target_obj in Patient_1 Patient_2
 	do
-		python run.py $target_obj --downsample=10 --corr_reg=${pp} --multiscale --logdir=${logdir} 2>&1 | tee ${logdir}/log_${target_obj}.txt
+		python run.py $target_obj --downsample=10 --multiscale --corr_reg=${pp} --logdir=${logdir} 2>&1 | tee ${logdir}/log_${target_obj}.txt
 	done
 done
