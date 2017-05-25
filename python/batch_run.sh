58de0937aca47f83fa30445bce8330544b369a96
yujin tang <lerrytang@gmail.com>
2017-05-25 17:07:02 +0900

    leave on out

--- python/batch_run.sh
+++ python/batch_run.sh
@@ -5,10 +5,11 @@ for pp in 0 0.00001 0.0001 0.001 0.01 0.1
 do
 	logdir="../log/baseline_"${pp}
 	mkdir -p ${logdir}
-	for target_obj in Dog_1 Dog_2 Dog_3 Dog_4 Dog_5
-	do
-		python run.py $target_obj --corr_reg=${pp} --logdir=${logdir} 2>&1 | tee ${logdir}/log_${target_obj}.txt
-	done
+	python run.py Dog_1 --n_folds=4 --corr_reg=${pp} --logdir=${logdir} 2>&1 | tee ${logdir}/log_Dog_1.txt
+	python run.py Dog_2 --n_folds=7 --corr_reg=${pp} --logdir=${logdir} 2>&1 | tee ${logdir}/log_Dog_2.txt
+	python run.py Dog_3 --n_folds=12 --corr_reg=${pp} --logdir=${logdir} 2>&1 | tee ${logdir}/log_Dog_3.txt
+	python run.py Dog_4 --n_folds=17 --corr_reg=${pp} --logdir=${logdir} 2>&1 | tee ${logdir}/log_Dog_4.txt
+	python run.py Dog_5 --n_folds=5 --corr_reg=${pp} --logdir=${logdir} 2>&1 | tee ${logdir}/log_Dog_5.txt
 	for target_obj in Patient_1 Patient_2
 	do
 		python run.py $target_obj --downsample=10 --corr_reg=${pp} --logdir=${logdir} 2>&1 | tee ${logdir}/log_${target_obj}.txt
@@ -21,10 +22,11 @@ for pp in 0 0.00001 0.0001 0.001 0.01 0.1
 do
 	logdir="../log/multiscale_"${pp}
 	mkdir -p ${logdir}
-	for target_obj in Dog_1 Dog_2 Dog_3 Dog_4 Dog_5
-	do
-		python run.py $target_obj --multiscale --corr_reg=${pp} --logdir=${logdir} 2>&1 | tee ${logdir}/log_${target_obj}.txt
-	done
+	python run.py Dog_1 --n_folds=4 --corr_reg=${pp} --logdir=${logdir} 2>&1 | tee ${logdir}/log_Dog_1.txt
+	python run.py Dog_2 --n_folds=7 --corr_reg=${pp} --logdir=${logdir} 2>&1 | tee ${logdir}/log_Dog_2.txt
+	python run.py Dog_3 --n_folds=12 --corr_reg=${pp} --logdir=${logdir} 2>&1 | tee ${logdir}/log_Dog_3.txt
+	python run.py Dog_4 --n_folds=17 --corr_reg=${pp} --logdir=${logdir} 2>&1 | tee ${logdir}/log_Dog_4.txt
+	python run.py Dog_5 --n_folds=5 --corr_reg=${pp} --logdir=${logdir} 2>&1 | tee ${logdir}/log_Dog_5.txt
 	for target_obj in Patient_1 Patient_2
 	do
 		python run.py $target_obj --downsample=10 --multiscale --corr_reg=${pp} --logdir=${logdir} 2>&1 | tee ${logdir}/log_${target_obj}.txt
